3a209139c0cd9830ac449d2b76e3cad0b8bb9a48
Hens743 <80003512+Hens743@users.noreply.github.com>
2023-03-06 16:55:39 +0100

    Update data_app.py

--- data_app.py
+++ data_app.py
@@ -5,5 +5,5 @@ df = pd.read_csv('NYC_most_pop.csv')
 
 edited_df = st.experimental_data_editor(df, num_rows="dynamic")
 
-favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+#favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
 st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
