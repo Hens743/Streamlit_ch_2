83771418fe39bbfe26eb941319062f800501d27b
Hens743 <80003512+Hens743@users.noreply.github.com>
2023-03-06 17:12:32 +0100

    Update data_app.py

--- data_app.py
+++ data_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv('NYC_most_pop.csv')
-st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
+st.markdown('Editable dataframes with st.experimental_data_editor (Streamlit 1.19')')
+
 edited_df = st.experimental_data_editor(df, num_rows="dynamic")
 
 #favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
