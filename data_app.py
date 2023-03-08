93e73a29b47e3ceb08f6be5a1d0538509b909704
Hens743 <80003512+Hens743@users.noreply.github.com>
2023-03-08 16:16:47 +0100

    Update data_app.py

--- data_app.py
+++ data_app.py
@@ -4,25 +4,35 @@ import pandas as pd
 #App config
 st.set_page_config(layout="centered")
 
+#Paths
+file_path1 = pd.read_csv("NYC_most_pop.csv")
+file_path2 = st.file_uploader("Select CSV file to upload", type=["csv"])
+
 #Data cache
 @st.cache_data()
 def load_data(file_path1):
-    df1 = pd.read_csv(file_path1)
-    return df1
+    default_data = pd.read_csv(file_path1)
+    return default_data
 
 @st.cache_data()
 def load_data(file_path2):
-    df2 = pd.read_csv(file_path2)
-    return df2
+    uploaded_data = pd.read_csv(file_path2)
+    return uploaded_data
 
-#Building
+#Titles
 st.header('Dataframes editor app')
-st.markdown ('Streamlit 1.19 - st.experimental_data_editor')
+st.markdown ('Made with: Streamlit 1.19 - st.experimental_data_editor')
 
+#Drop box
 option = st.selectbox('Select dataset',('Default dataset', 'Upload dataset'))
 st.write('You selected:', option)
 
-#Drop box
 if option == 'Upload dataset':
     st.file_uploader("Select CSV file to upload", type=["csv"])
 
+#Editable dataset
+df= [default_data,uploaded_data]
+st.experimental_data_editor(df, num_rows="dynamic")
+
+#favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+#st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
