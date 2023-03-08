cc2d401abd6d2d0b1507cef173142115609fddda
Hens743 <80003512+Hens743@users.noreply.github.com>
2023-03-08 13:07:10 +0100

    Update data_app.py

--- data_app.py
+++ data_app.py
@@ -9,7 +9,7 @@ def load_data(file_path):
   
 st.header('Editable dataframes with st.experimental_data_editor (Streamlit 1.19)')
 
-edited_df = st.experimental_data_editor(df, num_rows="dynamic")
+st.experimental_data_editor(df, num_rows="dynamic")
 
 #favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
 
