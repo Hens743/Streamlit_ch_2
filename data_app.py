288f45abd5db7e8251dd495a2d36825941e6277b
Hens743 <80003512+Hens743@users.noreply.github.com>
2023-03-08 14:17:18 +0100

    Update data_app.py

--- data_app.py
+++ data_app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas as pd
 
+#App config
 st.set_page_config(layout="centered")
 
+#Data cache
 @st.cache_data()
 def load_data(file_path1):
     df1 = pd.read_csv(file_path1)
@@ -13,12 +15,18 @@ def load_data(file_path2):
     df2 = pd.read_csv(file_path2)
     return df2
 
+#Building
 st.header('Dataframes editor app')
-
 st.markdown ('Streamlit 1.19 - st.experimental_data_editor')
-option = st.selectbox('Select dataset',('Default dataset', 'Upload dataset'))
-st.write('You selected:', option)
 
+option = st.selectbox('Select dataset',('Default dataset', 'Upload dataset'))
+#st.write('You selected:', option)
+
+#Drop box
+if st.selectbox = ('Upload dataset')
+    label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+        )
 file_path = st.file_uploader("Select CSV file to upload", type=["csv"])
 
 if file_path:
