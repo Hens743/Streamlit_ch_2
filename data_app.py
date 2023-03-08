402244d067d65ec4d20cba4b70515d65589e4ccf
Hens743 <80003512+Hens743@users.noreply.github.com>
2023-03-08 14:00:58 +0100

    Update data_app.py

--- data_app.py
+++ data_app.py
@@ -17,7 +17,21 @@ st.header('Dataframes editor app')
 
 st.markdown ('Streamlit 1.19 - st.experimental_data_editor')
 st.markdown ('Choose default dataset or Upload dataset')
-st.radio("Default dataset", options=["Yes", "No"], horizontal=1, index=1)
+
+file_path = st.file_uploader("Select CSV file to upload", type=["csv"])
+
+if file_path:
+    dataset = load_data(file_path)
+    top_menu = st.columns(3)
+    with top_menu[0]:
+        default = st.radio("Default dataset", options=["Yes", "No"], horizontal=1, index=1)
+    if default == "Yes":
+        with top_menu[1]:
+            sort_field = st.selectbox("Sort By", options=dataset.columns)
+        with top_menu[2]:
+            sort_direction = st.radio("Direction", options=["⬆️", "⬇️"], horizontal=True)
+        dataset = dataset.sort_values(by=sort_field, ascending=sort_direction == "⬆️", ignore_index=True)
+        
 file_path = st.file_uploader("Select CSV file to upload", type=["csv"])
 
 edited_df = st.experimental_data_editor(df, num_rows="dynamic")
