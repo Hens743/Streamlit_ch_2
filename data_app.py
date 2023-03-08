1c13375eb492b6e6b473d6ef70adf20706b4eee1
Hens743 <80003512+Hens743@users.noreply.github.com>
2023-03-08 13:17:10 +0100

    Update data_app.py

--- data_app.py
+++ data_app.py
@@ -3,14 +3,16 @@ import pandas as pd
 
 st.set_page_config(layout="centered")
 
+file_path = st.file_uploader("Select CSV file to upload", type=["csv"])
+
 @st.cache_data()
 def load_data(file_path):
     df = pd.read_csv(file_path)
     return df
   
-st.header('Editable dataframes with st.experimental_data_editor (Streamlit 1.19)')
+st.header('Editable dataframes app with st.experimental_data_editor (Streamlit 1.19)')
 
-#st.experimental_data_editor(df, num_rows="dynamic")
+st.experimental_data_editor(df, num_rows="dynamic")
 
 #favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
 
