28690ad7685fb1ddb2231d296b30785b4955e2a5
Hens743 <80003512+Hens743@users.noreply.github.com>
2023-03-10 13:48:54 +0100

    Update data_app.py

--- data_app.py
+++ data_app.py
@@ -8,7 +8,7 @@ st.set_page_config(layout="centered")
 #Data cache
 @st.cache_data()
 def load_data1(file_path1):
-    default_data = pd.read_csv(file_path1)
+    default_data = pd.read_csv("NYC_most_pop.csv")
     return default_data
 
 @st.cache_data()
@@ -21,15 +21,22 @@ st.header('Editable dataframes')
 st.markdown ('Made with: Streamlit 1.19 - st.experimental_data_editor')
 
 #Paths
-file_path1 = pd.read_csv("NYC_most_pop.csv")
-#file_path2 = st.file_uploader("Select CSV file to upload", type=["csv"])
 
+#file_path2 = st.file_uploader("Select CSV file to upload", type=["csv"])
+df= def load_data(url):
+    df = pd.read_csv(url)  # 👈 Download the data
+    return df
+
+df = load_data("https://github.com/plotly/datasets/raw/master/uber-rides-data1.csv")
+st.dataframe(df)
+
+st.button("Rerun")
 #Drop box
 option = st.selectbox('Select dataset',('Default dataset', 'Upload dataset'))
 st.write('You selected:', option)
 
 if option == 'Default dataset':
-    st.experimental_data_editor(default_data, num_rows="dynamic")
+    st.experimental_data_editor(file_path1, num_rows="dynamic")
 
 if option == 'Upload dataset':
     st.file_uploader("Select CSV file to upload", type=["csv"])
