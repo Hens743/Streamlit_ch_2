9db55a3398855abc53c931ccdb1911099709a02c
Hens743 <80003512+Hens743@users.noreply.github.com>
2023-03-23 13:24:39 +0100

    Update data_app.py
    add dwl box

--- data_app.py
+++ data_app.py
@@ -16,6 +16,10 @@ def load_data2(file_path2):
     uploaded_data = pd.read_csv(file_path2)
     return uploaded_data
 
+@st.cache_data()
+def convert_df(df):
+    return df.to_csv().encode('utf-8')
+
 #Titles
 st.header('Editable dataframes')
 st.markdown ('Made with: Streamlit 1.20 - st.experimental_data_editor')
@@ -35,31 +39,12 @@ if option == 'Upload dataset':
     if file_path2 is not None:
         st.experimental_data_editor(load_data2(file_path2), num_rows="dynamic")
 
-    
-# Multiple files
-#for f in uploaded_files:
-    #st.write(f)
-    
-#st.header('Multiple File Upload')
-#uploaded_files = st.file_uploader('Upload your files', accept_multiple_files=True)
-    
-#data_list = []
-#for f in uploaded_files:
-    #data = pd.read_csv(f)
-    #data_list.append(data)
+#Download box
+csv = convert_df(Your_data_df)
 
-#df = pd.concat(data_list)
-
-#Progress bar
-#progress_text = "Uploading in progress. Please wait."
-
-#if 
-#my_bar = st.progress(0, text=progress_text)
-
-#for percent_complete in range(100):
-    #time.sleep(0.1)
-    #my_bar.progress(percent_complete + 1, text=progress_text)
-
-#Editable dataset
-#df= load_data1(file_path1)
-#st.experimental_data_editor(df, num_rows="dynamic")
+st.download_button(
+    label="Download data as CSV",
+    data=csv,
+    file_name='Your_data.csv',
+    mime='text/csv',
+)
