ff63052c1a648767fb29244d54add8b2b8259f02
Hens743 <80003512+Hens743@users.noreply.github.com>
2023-03-23 13:02:32 +0100

    Update data_app.py

--- data_app.py
+++ data_app.py
@@ -8,7 +8,7 @@ st.set_page_config(layout="centered")
 #Data cache
 @st.cache_data()
 def load_data1(file_path1):
-    default_data = pd.read_csv("NYC_most_pop.csv")
+    default_data = pd.read_csv(file_path1)
     return default_data
 
 @st.cache_data()
@@ -21,18 +21,20 @@ st.header('Editable dataframes')
 st.markdown ('Made with: Streamlit 1.20 - st.experimental_data_editor')
 
 #Paths
-
-#file_path2 = st.file_uploader("Select CSV file to upload", type=["csv"])
+file_path1 = "NYC_most_pop.csv"
+file_path2 = st.file_uploader("Select CSV file to upload", type=["csv"])
 
 #Drop box
 option = st.selectbox('Select dataset',('Default dataset', 'Upload dataset'))
 st.write('You selected:', option)
 
 if option == 'Default dataset':
-    st.experimental_data_editor(load_data1, num_rows="dynamic")
+    st.experimental_data_editor(load_data1(file_path1), num_rows="dynamic")
 
 if option == 'Upload dataset':
-    st.file_uploader("Select CSV file to upload", type=["csv"])
+    if file_path2 is not None:
+        st.experimental_data_editor(load_data2(file_path2), num_rows="dynamic")
+
     
 # Multiple files
 #for f in uploaded_files:
